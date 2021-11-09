808d76f72c03783679c64ce2c131d42a043750cd
sonusainier <rohitsaini@lambdatest.com>
2021-11-09 10:11:55 +0530

    Cors middleware added

--- main.go
+++ main.go
@@ -58,6 +58,23 @@ func runListProv(*uc.Cmd) {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func runMain(*uc.Cmd) {
 	conf := NewConfig("config.json", "default.json")
 
@@ -65,6 +82,7 @@ func runMain(*uc.Cmd) {
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
+	r.Use(CORSMiddleware())
 
 	initTemplates(r, conf)
 	r.Static("/assets", "./assets")
